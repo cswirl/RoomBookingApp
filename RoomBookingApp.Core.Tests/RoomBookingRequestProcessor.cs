70dbcf46f14f023cfae36094027191058f4def85
cswirl <jay.angeles@protonmail.com>
2022-06-25 15:28:12 -0400

    Initial commit

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        internal RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            // Arrange
+            var bookingRequest = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                DateTime = new DateTime(2021, 10, 20)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            // Act
+            RoomBookingResult result = processor.BookRoom(bookingRequest);
+
+            //Assert
+            Assert.NotNull(result);
+
+
+        }
+
+        
+    }
+}
